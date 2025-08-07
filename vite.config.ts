22aced16dfbe42faa8557252794e437e94134fcc
naru18vr <nar812u@gmail.com>
2025-08-07 01:26:12 -0400

    Update vite.config.ts base option to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: "/dokkai-training2-app/",
+      base: "./",
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
